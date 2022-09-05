bd277860cc62bf73c25b1547e0bfb4f46b058529
Yuan Xu <ariayxu@umd.edu>
2022-09-05 00:38:55 -0400

    Add answer entity

---
+++ src/main/java/com/trivia/trivia/entity/AnswerEntity.java
@@ -0,0 +1,80 @@
+package com.trivia.trivia.entity;
+
+import org.hibernate.annotations.TypeDef;
+import org.hibernate.annotations.TypeDefs;
+
+import javax.persistence.*;
+import java.util.UUID;
+
+@Entity
+@Table(name="answers")
+public class AnswerEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name="answer_id")
+    private long answerId;
+
+    @Column(name="user_id")
+    private UUID userId;
+
+    @Column(name="question_id")
+    private int questionId;
+
+    @Column(name="header")
+    private String header;
+
+    @Column(name="answer", columnDefinition = "text[]")
+    private String[] answer;
+
+    public AnswerEntity(){
+    }
+
+    public AnswerEntity(long answerId, UUID userId, int questionId, String header, String[] answer) {
+        this.answerId = answerId;
+        this.userId = userId;
+        this.questionId = questionId;
+        this.header = header;
+        this.answer = answer;
+    }
+
+    public long getAnswerId() {
+        return answerId;
+    }
+
+    public void setAnswerId(long answerId) {
+        this.answerId = answerId;
+    }
+
+    public UUID getUserId() {
+        return userId;
+    }
+
+    public void setUserId(UUID userId) {
+        this.userId = userId;
+    }
+
+    public int getQuestionId() {
+        return questionId;
+    }
+
+    public void setQuestionId(int questionId) {
+        this.questionId = questionId;
+    }
+
+    public String getHeader() {
+        return header;
+    }
+
+    public void setHeader(String header) {
+        this.header = header;
+    }
+
+    public String[] getAnswer() {
+        return answer;
+    }
+
+    public void setAnswer(String[] answer) {
+        this.answer = answer;
+    }
+}
