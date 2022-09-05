705e6633867223a719f1afbf758b9771c3f75f75
Yuan Xu <ariayxu@umd.edu>
2022-09-05 00:57:35 -0400

    Add answer repository

---
+++ src/main/java/com/trivia/trivia/repository/AnswerRepository.java
@@ -0,0 +1,9 @@
+package com.trivia.trivia.repository;
+
+import com.trivia.trivia.entity.AnswerEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface AnswerRepository extends JpaRepository<AnswerEntity, Long> {
+}
